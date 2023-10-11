abc7d8337c2d168269d8699a2bb43f77b091c188
SachinSureshBusiness <83098929+SureshS13@users.noreply.github.com>
2023-10-11 14:24:07 -0500

    Initial commit

---
+++ src/main/java/com/sachinsuresh/fetchapidemo/FetchApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsuresh.fetchapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsuresh/fetchapidemo/FetchApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsuresh.fetchapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
